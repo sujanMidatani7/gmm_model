e5f82b6162c991bfc20aaf5742e23e3661ab69a0
Sujan midatani <105052933+sujanMidatani7@users.noreply.github.com>
2023-04-26 17:26:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,14 +26,14 @@ def analyze_audio(file):
         return
 
     # Encode the audio signal using the x-vector model
-    mfcc1 = librosa.feature.mfcc(y=waveform.numpy(), sr=16000, n_mfcc=20, n_fft=4096, hop_length=11)
+    mfcc1 = librosa.feature.mfcc(y=waveform.numpy(), sr=16000, n_mfcc=20, n_fft=2048, hop_length=11)
     delta_mfcc1 = librosa.feature.delta(mfcc1)
     delta2_mfcc1 = librosa.feature.delta(mfcc1, order=2)
     ddmfcc1 = np.vstack((mfcc1, delta_mfcc1, delta2_mfcc1))
     ddmfcc1 = (ddmfcc1 - np.mean(ddmfcc1)) / np.std(ddmfcc1)
 #     st.write(ddmfcc1.shape)
     X1 = np.reshape(ddmfcc1.T, (ddmfcc1.shape[1]*ddmfcc1.shape[0]*ddmfcc1.shape[2], 1))
-    n_components = 23
+    n_components = 25
 
     gmm1 = GaussianMixture(n_components=n_components, covariance_type='diag')
     gmm1.fit(X1)
